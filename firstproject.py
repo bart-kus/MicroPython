b93c7d854a6a2b841e581866032bce9f854f9975
bart-kus <kusmonbar@gmail.com>
2022-12-19 21:15:50 +0000

    anothercommit

---
+++ firstproject.py
@@ -0,0 +1,20 @@
+import machine
+import utime
+
+led = machine.Pin(15, machine.Pin.OUT)
+led2 = machine.Pin(14, machine.Pin.OUT)
+led3 = machine.Pin("LED", machine.Pin.OUT)
+
+while True:
+    led.on()
+    led2.off()
+    led3.off()
+    utime.sleep(2)
+    led.off()
+    led2.on()
+    led3.off()
+    utime.sleep(2)
+    led.off()
+    led2.off()
+    led3.on()
+    utime.sleep(2)
